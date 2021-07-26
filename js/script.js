c0f192e6fe450f6fc72fa1b77777012580ef62d1
hsanJaballi <hsan.jaballi@gmail.com>
2021-07-26 23:35:45 +0100

    init: js::script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const init = () => {
+    const fetcher = (url) => fetch(url).then(r => r.json());
+
+    const repositoriesURL = `https://api.github.com/users/hsanJaballi/repos`;
+
+    const onSuccess = (response) => {
+
+        const target = document.getElementById(`repositories-items`);
+
+        console.log(response);
+        
+        response.map((o, i) => {
+            const el = document.createElement(`repository-item`);
+            el.setAttribute(`rname`, o.name);
+            el.setAttribute(`rdescription`, o.description);
+            target.appendChild(el);
+        })
+
+    }
+
+    const onFailure = (reason) => {
+        console.warn(reason.message);
+    }
+
+    fetcher(repositoriesURL).then(onSuccess).catch(onFailure);
+}
+
+window.onload = init
